09fb0c963558c33a62159e959cbd5b2f3d1a8547
Stefano Miceli <Rende>
2023-02-08 19:05:26 +0100

    componente creautente e componente utentelist

--- src/app/crea-utente/crea-utenti.component.ts
+++ src/app/crea-utente/crea-utenti.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Utente} from "../utente";
+import {UtenteServiceService} from "../utente.service.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-crea-utenti',
   templateUrl: './crea-utenti.component.html',
   styleUrls: ['./crea-utenti.component.scss']
 })
-export class CreaUtentiComponent {
+export class CreaUtentiComponent implements OnInit{
+  utente: Utente = new Utente();
+  submitted = false;
+  constructor(private es:UtenteServiceService, private router: Router) {} ngOnInit() {
+  }
 
+  newUtente(): void{
+    this.submitted = false;
+    this.utente = new Utente();
+  }
+  save(){
+    this.es.creaUtente(this.utente).subscribe(()=>{
+      this.utente = new Utente();
+      this.gotoList();
+    });
+  }
+
+
+  private gotoList() {
+    this.router.navigate(["/utente"]);
+  }
 }
