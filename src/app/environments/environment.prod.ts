84552c287b2bf47699de19b2b5546e8ddb655d82
Stefano Miceli <Rende>
2023-03-28 18:22:11 +0200

    123

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAivb0EXv6izUnJPxBg5_ueVfUMib-OsNU",
+    authDomain: "promemoria-c23cb.firebaseapp.com",
+    projectId: "promemoria-c23cb",
+    storageBucket: "promemoria-c23cb.appspot.com",
+    messagingSenderId: "649828189225",
+    appId: "1:649828189225:web:edbb6b04735cf7325a4838",
+    measurementId: "G-KNXFVND4T0",
+  }
+};
